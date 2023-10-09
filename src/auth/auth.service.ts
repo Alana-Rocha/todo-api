b4fb339c4069f44a21857aa055bc31afda1bf879
Alana-Rocha <alanarodrigues.rocha1@gmail.com>
2023-10-09 19:17:31 -0300

    create auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,52 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { AuthDto } from './dto/create-auth.dto';
+import { compareSync, hashSync } from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  login(authDto: AuthDto) {
-    if(!authDto.nome || !authDto.senha ) throw new BadRequestException('Nome e senha são obrigatórios');
-    
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
+  async login(authDto: AuthDto) {
+    const usuario = await this.prisma.usuarios.findUnique({
+      where: { usr_nome: authDto.nome },
+    });
+
+    if (!usuario) {
+      throw new BadRequestException('Usuário não cadastrado');
+    }
+
+    const senhaValida = compareSync(authDto.senha, usuario.usr_senha);
+    if (!senhaValida) {
+      throw new BadRequestException('Senha inválida');
+    }
+
+    const payload = {
+      nome: usuario.usr_nome,
+    };
+
+    return { token: this.jwtService.sign(payload), payload };
   }
 
-  cadastro(authDto: AuthDto) {
-    
+  async cadastro(authDto: AuthDto) {
+    const usuario = await this.prisma.usuarios.findUnique({
+      where: { usr_nome: authDto.nome },
+    });
+
+    if (usuario) {
+      throw new BadRequestException('Usuário já existe');
+    }
+    const usuarioCadastrado = await this.prisma.usuarios.create({
+      data: { usr_nome: authDto.nome, usr_senha: hashSync(authDto.senha, 10) },
+    });
+
+    const payload = {
+      nome: usuarioCadastrado.usr_nome,
+    };
+
+    return { token: this.jwtService.sign(payload), payload };
   }
 }
--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,4 +1,13 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthDto {
+  @IsString()
   nome: string;
+
+  @MinLength(8, {message: 'A senha deve ter no mínimo 8 caracteres'})
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Senha muito fraca',
+  })
+  @IsString()
   senha: string;
 }
