50962882ba50c7595139c02c3ec8cc550ed99f16
Alana-Rocha <alanarodrigues.rocha1@gmail.com>
2023-10-10 17:13:11 -0300

    create CRUD tarefas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { TarefasModule } from './tarefas/tarefas.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, PrismaModule],
+  imports: [ConfigModule.forRoot(), AuthModule, PrismaModule, TarefasModule],
 })
 export class AppModule {}
